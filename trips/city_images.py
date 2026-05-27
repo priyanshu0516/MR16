1fdbf42b668f6f6f8b68afb05e5468aa46108ad3
Priyanshu Tripathi <ankittripathi@MacBookAir.lan>
2026-05-27 15:31:24 +0530

    Added city images

--- trips/city_images.py
+++ trips/city_images.py
@@ -2,4 +2,8 @@ CITY_IMAGES = {
     "jaipur": "images/jaipur.jpg",
     "nainital": "images/nainital.jpg",
     "bhopal": "images/bhopal.jpg",
+    "delhi": "images/delhi.jpg",
+    "pune": "images/pune.jpg",
+    "chhatarpur": "images/chhatarpur.jpg",
+    "mumbai": "images/mumbai.jpg",
 }
